04e9e3377926b5baa95641d4113807467672acb6
Yurchenkopi <heppie@yandex.ru>
2022-01-14 17:04:59 +0300

    на ноут

---
+++ src/main/java/ru/job4j/inheritance/Test.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+        String name = "null";
+        String str = "null";
+        boolean rsl;
+        if (name.equals(str)) {
+            rsl = true;
+        } else rsl = false;
+        System.out.println(rsl);
+    }
+}
