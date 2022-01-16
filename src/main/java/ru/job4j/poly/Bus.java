cf648a0fd544baf98be87f13ffceaa5996ac310f
Pavel <heppie@yandex.ru>
2022-01-16 14:22:25 +0300

    3.2. Зачем нужно приведение типов [#310019]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,6 +1,6 @@
 package ru.job4j.poly;
 
-public class Bus implements Transport {
+public class Bus implements Transport, Vehicle {
     @Override
     public void drive() {
         System.out.println("Тррррр");
@@ -14,5 +14,10 @@ public class Bus implements Transport {
     @Override
     public int refuel(int litresFuel) {
         return litresFuel * 50;
+    }
+
+    @Override
+    public void move() {
+        System.out.println(getClass().getSimpleName() + " двигается по скоростным трассам.");
     }
 }
