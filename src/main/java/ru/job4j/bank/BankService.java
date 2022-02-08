e72ba908432ba7a3fcc1c9429f0d6958eede442a
Pavel <heppie@yandex.ru>
2022-02-08 19:39:22 +0300

    3. Банковские переводы [#10038 #261053]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -14,7 +14,7 @@ public class BankService {
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        if (user != null) {
+        if (user != null && !users.get(user).contains(account)) {
             users.get(user).add(account);
         }
     }
