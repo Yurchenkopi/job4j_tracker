420c89bffbe6cc62f5d38e75e20c7ecd2ae74f36
Yurchenkopi <heppie@yandex.ru>
2022-02-11 07:58:55 +0300

    3. Компаратор для строк [#35008 #262901]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,43 +5,11 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int rsl = 0;
-        int minLength;
-        boolean isEqual = false;
-        boolean rightLessLeft = false;
-        boolean finRsl = false;
-        char[] leftArray = new char[left.length()];
-        char[] rightArray = new char[right.length()];
-        for (int i = 0; i < left.length(); i++) {
-            leftArray[i] = left.charAt(i);
-        }
-        for (int i = 0; i < right.length(); i++) {
-            rightArray[i] = right.charAt(i);
-        }
-        if (left.length() <= right.length()) {
-            minLength = left.length();
-            isEqual = left.length() == right.length();
-        } else {
-            minLength = right.length();
-            rightLessLeft = true;
-        }
-        for (int i = 0; i < minLength; i++) {
-            int tempRsl = Character.compare(leftArray[i], rightArray[i]);
-            if (tempRsl != 0) {
-                rsl = tempRsl;
-                finRsl = true;
-                break;
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
             }
         }
-        if (!finRsl) {
-            if (isEqual) {
-                rsl = 0;
-            } else if (rightLessLeft) {
-                rsl = left.charAt(minLength);
-            } else {
-                rsl = -right.charAt(minLength);
-            }
-        }
-        return rsl;
+        return Integer.compare(left.length(), right.length());
     }
 }
