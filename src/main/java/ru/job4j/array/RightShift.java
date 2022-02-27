a2c374322c4421251b39989ac3e421c6ab59683e
Pavel <heppie@yandex.ru>
2022-02-27 13:26:31 +0300

    1.2. Функциональный интерфейс [#247497 #269919]

--- src/main/java/ru/job4j/array/RightShift.java
+++ src/main/java/ru/job4j/array/RightShift.java
@@ -12,7 +12,6 @@ public class RightShift {
         System.out.println(Arrays.toString(nums));
     }
 
-    // метод делает сдвиг с шагом 1
     private static void shift(int[] nums) {
         int buf = nums[nums.length - 1];
         for (int i = 0; i < nums.length - 1; i++) {
--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.*;
 
-public class FunctionalInterfaces {
+public class FunctionalInterfaces  {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
         BiConsumer<Integer, String> biCon = (key, str) -> map.put(key, str);
