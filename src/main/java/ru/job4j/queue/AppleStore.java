f8629beef978d95eea78544e107922011934439d
Yurchenkopi <heppie@yandex.ru>
2023-03-10 13:56:45 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,31 @@
+package ru.job4j.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        Queue<Customer> temp = new LinkedList<>(queue);
+        for (int i = 0; i < count - 1; i++) {
+            temp.poll();
+        }
+        return temp.element().name();
+    }
+
+    public String getFirstUpsetCustomer() {
+        Queue<Customer> temp = new LinkedList<>(queue);
+        for (int i = 0; i < count; i++) {
+            temp.poll();
+        }
+        return temp.element().name();
+    }
+}
